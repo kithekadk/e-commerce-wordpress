c4a322d976eb68359bad7c7772146d7c50507378
unknown <dankinyi99@gmail.com>
2022-12-19 17:37:07 +0300

    added admin sub pages

--- wp-content/themes/kithekacustom/inc/function-admin.php
+++ wp-content/themes/kithekacustom/inc/function-admin.php
@@ -8,11 +8,20 @@ ADMIN PAGE
 */
 
 function kithekatheme_add_admin_page(){
-    add_menu_page('Kitheka theme options', 'KithekaTheme', 'manage_options', 'kithekadk-theme', 'kithekadk_theme_create_page');
+    //Create admin page
+    add_menu_page('Kitheka Theme options', 'KithekaTheme', 'manage_options', 'kithekadk_theme', 'kithekadk_theme_create_page');
+
+    //CReate admin sub pages
+    add_submenu_page('kithekadk_theme', 'Kitheka Theme options', 'Settings', 'manage_options', 'kithekadk_theme', 'kithekadk_theme_create_page');
+    add_submenu_page('kithekadk_theme', 'Kitheka Theme options2', 'Settings2', 'manage_options', 'kithekadk_theme2', 'kithekadk_theme_create_page');
 }
 
 add_Action ('admin_menu', 'kithekatheme_add_admin_page');
 function kithekadk_theme_create_page(){
     //generation of our admin page
+    require_once( get_template_directory() . '/inc/templates/kithekadk-admin.php');
+}
+
+function kithekadk_theme_settings_page(){
 
 }
---
+++ wp-content/themes/kithekacustom/inc/templates/kithekadk-admin.php
@@ -0,0 +1,3 @@
+<h1>Kitheka Theme Options</h1>
+<h3 class="title">Manage Options</h3>
+<p>Customize the default Wordpress Appearance Options</p>
